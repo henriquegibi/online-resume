d37e125ecbede73615e5458be863c949458ee2ac
Henrique Gibi <henriquedepadua@yahoo.com.br>
2024-09-23 14:32:39 -0300

    Separate javascript to a dedicated file

---
+++ js/resume.js
@@ -0,0 +1,21 @@
+
+var btn1 = document.getElementById("show1");
+btn1.addEventListener("click", function (event) {
+    document.getElementById("toggle1").style.display = "inherit";
+    event.target.style.display = "none";
+})
+var btn2 = document.getElementById("show2");
+btn2.addEventListener("click", function (event) {
+    document.getElementById("toggle2").style.display = "inherit";
+    event.target.style.display = "none";
+})
+var btn3 = document.getElementById("show3");
+btn3.addEventListener("click", function (event) {
+    document.getElementById("toggle3").style.display = "inherit";
+    event.target.style.display = "none";
+})
+var btn4 = document.getElementById("show4");
+btn4.addEventListener("click", function (event) {
+    document.getElementById("toggle4").style.display = "inherit";
+    event.target.style.display = "none";
+})
