0fcaaf4f02206e7150d1f55423711640bc616c65
Henrique Gibi <henriquedepadua@yahoo.com.br>
2024-09-23 18:49:56 -0300

    Started sql generator project editing java back-end

---
+++ java-app-sql-generator/src/main/java/click/henriquegibi/sql_generator/SqlGeneratorApplication.java
@@ -0,0 +1,13 @@
+package click.henriquegibi.sql_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlGeneratorApplication.class, args);
+	}
+
+}
---
+++ java-app-sql-generator/src/test/java/click/henriquegibi/sql_generator/SqlGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package click.henriquegibi.sql_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
